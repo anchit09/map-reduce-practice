afcc036c97e91d6ba1ce67ca2d29ee143ed32b14
anchit09 <anchit.jatana@target.com>
2018-03-26 14:26:51 -0700

    Adding mapreduce example

---
+++ src/main/java/com/anchit/practice/WordCount.java
@@ -0,0 +1,69 @@
+package com.anchit.practice;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+/**
+ * Created by anchit09 on 3/26/18.
+ */
+
+public class WordCount {
+
+    public static class MyMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private static final IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        @Override
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(word, one);
+            }
+        }
+    }
+
+    public static class MyReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        JobConf conf = new JobConf(WordCount.class);
+        conf.setJobName("WordCount");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(MyMapper.class);
+        conf.setCombinerClass(MyReducer.class);
+        conf.setReducerClass(MyReducer.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        conf.setNumReduceTasks(3);
+
+        JobClient.runJob(conf);
+    }
+}
